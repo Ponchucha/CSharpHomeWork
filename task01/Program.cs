bcd3a7ef021e8492619d667d126d00e84edd02c3
Anna <ponchucha@yandex.ru>
2022-12-01 13:27:47 +0300

    Add task 01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine ("Введите первое число");
+string numberOneStr = Console.ReadLine();
+int numberOne = Convert.ToInt32(numberOneStr);
+
+Console.WriteLine ("Введите первое число");
+string numberTwoStr = Console.ReadLine();
+int numberTwo= Convert.ToInt32(numberTwoStr);
+
+if (numberOne>numberTwo)
+{
+    Console.WriteLine ($"{numberOne} is larger");
+}
+else
+    if (numberOne<numberTwo)
+    {
+        Console.WriteLine ($"{numberTwo} is larger");
+    
+    }
+    else Console.WriteLine ("nubers are equal");
